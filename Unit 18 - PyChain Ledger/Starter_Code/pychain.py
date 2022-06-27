17e01a59e7cd104dc752e8df8681ed08ff1828a7
tarikthomas <98078463+tarikthomas@users.noreply.github.com>
2022-06-27 12:17:00 +1000

    Update pychain.py

--- Unit 18 - PyChain Ledger/Starter_Code/pychain.py
+++ Unit 18 - PyChain Ledger/Starter_Code/pychain.py
@@ -51,7 +51,7 @@ class Record:
 # Note that you’ll use this new `Record` class as the data type of your `record` attribute in the next section.
 
 
-# @TODO
+
 # Create a Record Data Class that consists of the `sender`, `receiver`, and
 # `amount` attributes
 # YOUR CODE HERE
@@ -71,7 +71,6 @@ class Record:
 @dataclass
 class Block:
 
-    # @TODO
     # Rename the `data` attribute to `record`, and set the data type to `Record`
     record: Record
 
@@ -169,20 +168,20 @@ pychain = setup()
 # 4. Add an input area where you can get a value for `amount` from the user.
 # 5. As part of the Add Block button functionality, update `new_block` so that `Block` consists of an attribute named `record`, which is set equal to a `Record` that contains the `sender`, `receiver`, and `amount` values. The updated `Block`should also include the attributes for `creator_id` and `prev_hash`.
 
-# @TODO:
+        
 # Delete the `input_data` variable from the Streamlit interface.
 
 
-# @TODO:
+
 # Add an input area where you can get a value for `sender` from the user.
 input_sender = st.text_area('sender')
 
 
-# @TODO:
+
 # Add an input area where you can get a value for `receiver` from the user.
 input_receiver = st.text_area('receiver')
 
-# @TODO:
+
 # Add an input area where you can get a value for `amount` from the user.
 input_amount = st.text_area('amount')
 
@@ -190,7 +189,7 @@ if st.button("Add Block"):
     prev_block = pychain.chain[-1]
     prev_block_hash = prev_block.hash_block()
 
-    # @TODO
+
     # Update `new_block` so that `Block` consists of an attribute named `record`
     # which is set equal to a `Record` that contains the `sender`, `receiver`,
     # and `amount` values
